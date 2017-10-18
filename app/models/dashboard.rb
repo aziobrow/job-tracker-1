dbfb46a0efb47ce0e6b99181963d6191b1956b84
Aurora Ziobrowski <aziobrow@gmail.com>
2017-10-17 22:41:53 -0600

    dashboard is in progress. working on top 3 companies by average level of interest

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -6,8 +6,15 @@ class Dashboard < ApplicationRecord
     .count
   end
 
-  def average_level_of_interest_by_company (company model, for each)
-    jobs
+  # def average_level_of_interest_by_company (company model, for each)
+  #   joins(:jobs)
+  #     .select("average(level_of_interest) AS avg_interest, company.id")
+  #     .group("company.id")
+  #     .order("avg_interest")
+  # end
+
+  def average_level_of_interest_by_company
     .average(:level_of_interest)
+    .limit(3)
   end
 end
